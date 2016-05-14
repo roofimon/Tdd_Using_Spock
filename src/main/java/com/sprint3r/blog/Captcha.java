39e03530b9eea11b1c09a658b78b3d9459594b94
roofimon <roofimon@gmail.com>
2016-05-14 14:20:30 +0700

    Ignore DS_Store

--- src/main/java/com/sprint3r/blog/Captcha.java
+++ src/main/java/com/sprint3r/blog/Captcha.java
@@ -1,19 +1,7 @@
 package com.sprint3r.blog;
 
 /**
- * Created by roof on 11/11/15.
+ * Created by roofimon on 5/14/2016 AD.
  */
 public class Captcha {
-    private int left, pattern;
-    public Captcha(int pattern, int left, int operator, int right) {
-            this.left = left;
-            this.pattern = pattern;
-    }
-
-    public String left() {
-        String[] numericString = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-        if (this.pattern==1)
-            return numericString[this.left-1];
-        return String.valueOf(this.left);
-    }
 }
