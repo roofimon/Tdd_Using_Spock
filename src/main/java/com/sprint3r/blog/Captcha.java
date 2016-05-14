1abd9add7a40e8f49451f96b9154a147b32d6e39
roofimon <roofimon@gmail.com>
2016-05-14 14:20:50 +0700

    Finish simple captcha

--- src/main/java/com/sprint3r/blog/Captcha.java
+++ src/main/java/com/sprint3r/blog/Captcha.java
@@ -4,4 +4,44 @@ package com.sprint3r.blog;
  * Created by roofimon on 5/14/2016 AD.
  */
 public class Captcha {
+    private int left;
+    private int pattern;
+    private int right;
+    private int operator;
+    String[] arr = {"one","two","three","four","five","six","seven","eight","nine"};
+
+    public Captcha(int pattern, int left, int operator, int right){
+        this.left = left;
+        this.pattern = pattern;
+        this.right = right;
+        this.operator = operator;
+    }
+    public String left(){
+        if (pattern == 1) {
+            return String.valueOf(left);
+        }
+        return arr[left - 1];
+    }
+
+    public String right(){
+        if (pattern == 1) {
+            return arr[right - 1];
+        }
+        return String.valueOf(right);
+    }
+
+    public String operator() {
+        if (operator == 1) {
+            return "+";
+        }
+        if (operator == 2) {
+            return "*";
+        }
+        return "-";
+    }
+
+    public String toString() {
+        return left() + " " + operator() + " " + right();
+    }
+
 }
--- src/test/groovy/com/sprint3r/blog/CaptchaTest.groovy
+++ src/test/groovy/com/sprint3r/blog/CaptchaTest.groovy
@@ -3,31 +3,146 @@ package com.sprint3r.blog
 import spock.lang.Specification
 
 class CaptchaTest extends Specification{
-    def "First pattern left operand shold be numeric string"() {
+    def "Returned string should be 'three - 1' "(){
         setup:
-            def captcha = new Captcha(pattern, left, operator, right)
+            def captcha = new Captcha(2, 3, 3, 1)
 
-        expect:
-            captcha.left() == result
+        when:
+            def combined = captcha.toString()
 
-        where:
-            pattern | left | operator | right | result
-            1       | 1    | 1        | 1     | "One"
-            1       | 2    | 1        | 1     | "Two"
-            1       | 9    | 1        | 1     | "Nine"
+        then:
+            combined == "three - 1"
     }
-
-    def "Second pattern left operand shold be numeric"() {
+    def "Returned string should be '1 + one' "(){
         setup:
-            def captcha = new Captcha(pattern, left, operator, right)
+            def captcha = new Captcha(1, 1, 1, 1)
 
-        expect:
-            captcha.left() == result
+        when:
+            def combined = captcha.toString()
 
-        where:
-            pattern | left | operator | right | result
-            2       | 1    | 1        | 1     | "1"
-            2       | 2    | 1        | 1     | "2"
-            2       | 9    | 1        | 1     | "9"
+        then:
+            combined == "1 + one"
+    }
+    def "Operator should be minus"(){
+        setup:
+            def captcha = new Captcha(1, 1, 3, 1)
+
+        when:
+            def operator = captcha.operator()
+
+        then:
+            operator == "-"
+    }
+    def "Operator should be multiply"(){
+        setup:
+            def captcha = new Captcha(1, 1, 2, 1)
+
+        when:
+            def operator = captcha.operator()
+
+        then:
+            operator == "*"
+    }
+    def "Operator should be plus"(){
+        setup:
+            def captcha = new Captcha(1, 1, 1, 1)
+
+        when:
+            def operator = captcha.operator()
+
+        then:
+            operator == "+"
+    }
+    def "Second pattern right operand should be 9"(){
+        setup:
+            def captcha = new Captcha(2, 1, 1, 9)
+
+        when:
+            def right = captcha.right()
+
+        then:
+            right == "9"
+    }
+    def "Second pattern right operand should be 1"(){
+        setup:
+            def captcha = new Captcha(2, 1, 1, 1)
+
+        when:
+            def right = captcha.right()
+
+        then:
+            right == "1"
+    }
+    def "First pattern right operand should be nine"(){
+        setup:
+            def captcha = new Captcha(1, 1, 1, 9)
+
+        when:
+            def right = captcha.right()
+
+        then:
+            right == "nine"
+    }
+    def "First pattern right operand should be one"(){
+        setup:
+            def captcha = new Captcha(1, 1, 1, 1)
+
+        when:
+            def right = captcha.right()
+
+        then:
+            right == "one"
+    }
+    def "Second pattern left operand should be nine"(){
+        setup:
+            def captcha = new Captcha(2, 9, 1, 1)
+
+        when:
+            def left = captcha.left()
+
+        then:
+            left == "nine"
+    }
+    def "Second pattern left operand should be one"() {
+        setup:
+            def captcha = new Captcha(2, 1, 1, 1)
+
+        when:
+            def left = captcha.left()
+
+        then:
+            left == "one"
+    }
+    def "First pattern left operand should be 1"() {
+        setup:
+            def captcha = new Captcha(1, 1, 1, 1)
+
+        when:
+            def left = captcha.left()
+
+        then:
+            left == "1"
+
+    }
+    def "First pattern left operand should be 2"(){
+        setup:
+            def captcha = new Captcha(1, 2, 1, 1)
+
+        when:
+            def left = captcha.left()
+
+        then:
+            left == "2"
+
+    }
+    def "First pattern left operand should be 9"(){
+        setup:
+            def captcha = new Captcha(1, 9, 1, 1)
+
+        when:
+            def left = captcha.left()
+
+        then:
+            left == "9"
     }
 }
